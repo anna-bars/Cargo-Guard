76c02f40488194b989442daad7b36ecd20fb1d6b
Anna Bars <abarseghyan863@gmail.com>
2026-01-14 02:03:47 +0400

    FIXEX the mobile version of Quotes Expiration widget

--- src/app/components/charts/ConversionChart.tsx
+++ src/app/components/charts/ConversionChart.tsx
@@ -290,7 +290,7 @@ export const ConversionChart = () => {
 
   return (
     <div className="flex flex-col justify-between border border-[#d1d1d154] bg-[#fdfdf8cf] rounded-2xl p-4 h-full w-full quote-conversion performance-section hover:shadow-sm transition-shadow duration-300">
-      <div className="mb-[4px] gap-2 flex-col flex justify-between">
+      <div className="mb-[4px] gap-2 md:flex-col flex justify-between flex-row">
         <h3 className="font-montserrat text-[18px] font-normal text-black action-title max-[1024px]:text-[14px]">
           Quote Conversion Rate
         </h3>
